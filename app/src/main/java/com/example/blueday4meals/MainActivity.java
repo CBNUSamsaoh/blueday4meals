a151d61ad81f4ba1dc88d6afafc81f8817da91db
lee jin gyeong <jenjennie707@gmail.com>
2023-05-13 19:21:25 +0900

    카카오 맵 API 연동

--- app/src/main/java/com/example/blueday4meals/MainActivity.java
+++ app/src/main/java/com/example/blueday4meals/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.blueday4meals;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
+    MapView mapView;
+    RelativeLayout mapViewContainer;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapViewContainer = (RelativeLayout)findViewById(R.id.map_view);
+        mapView = new MapView(this);
+        mapViewContainer.addView(mapView);
     }
 }
-
