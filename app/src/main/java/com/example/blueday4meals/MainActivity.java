f45236a610fbba0785c33a157fc0c9b3bb3ec581
lee jin gyeong <jenjennie707@gmail.com>
2023-05-16 16:22:53 +0900

    네이버맵 api 추가

--- app/src/main/java/com/example/blueday4meals/MainActivity.java
+++ app/src/main/java/com/example/blueday4meals/MainActivity.java
@@ -1,23 +1,16 @@
 package com.example.blueday4meals;
 
-import android.os.Bundle;
-import android.widget.RelativeLayout;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import net.daum.mf.map.api.MapView;
+
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    MapView mapView;
-    RelativeLayout mapViewContainer;
-
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mapViewContainer = (RelativeLayout)findViewById(R.id.map_view);
-        mapView = new MapView(this);
-        mapViewContainer.addView(mapView);
+
+
     }
 }
