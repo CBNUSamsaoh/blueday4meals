2990b45a4f56ccdc846df207de777ad4d833e905
Kang Tae Kwang <animalnara@gmail.com>
2023-05-25 11:46:53 +0900

    네이버지도_마커적용버전
    네이버지도_마커적용버전입니다.

---
+++ app/src/main/java/com/example/blueday4meals/NaverMapData.java
@@ -0,0 +1,54 @@
+package com.example.blueday4meals;
+
+import com.google.gson.annotations.SerializedName;
+
+public class NaverMapData {
+    @SerializedName("serialNum")
+    private int serialNum;
+    @SerializedName("storeName") // php 파일과 이름이 같아야 함.
+    private String storeName;
+    @SerializedName("storeCategory")
+    private String storeCategory;
+    @SerializedName("storeLat")
+    private double storeLat;
+    @SerializedName("storeLnt")
+    private double storeLnt;
+    @SerializedName("storePhonenum")
+    private String storePhonenum;
+    @SerializedName("storeAddr")
+    private String storeAddr;
+    @SerializedName("storeMenu")
+    private String storeMenu;
+
+    public int getSerialNum() {
+        return serialNum;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public String getStoreCategory() {
+        return storeCategory;
+    }
+
+    public double getStoreLat() {
+        return storeLat;
+    }
+
+    public double getStoreLnt() {
+        return storeLnt;
+    }
+
+    public String getStorePhonenum() {
+        return storePhonenum;
+    }
+
+    public String getStoreAddr() {
+        return storeAddr;
+    }
+
+    public String getStoreMenu() {
+        return storeMenu;
+    }
+}
---
+++ app/src/main/java/com/example/blueday4meals/NaverMapItem.java
@@ -0,0 +1,8 @@
+package com.example.blueday4meals;
+
+import java.util.List;
+
+public class NaverMapItem {
+    @SerializedName("data")
+    public List<NaverMapData> data;
+}
